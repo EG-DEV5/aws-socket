c7fb6418520428abf9ba0a0be858eab6cc070f0e
Mohamed Sabry <egypt.team.dev5@gmail.com>
2024-11-25 14:52:18 +0200

    feat: :sparkles: add a produce namespace to recive devices update

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,27 +1,27 @@
-import crypto from 'crypto'
+// import crypto from 'crypto'
 
-const isAuthenticatedConsumer = function (token: string): boolean {
-  try {
-    decryptApiKey(token, process.env.consumer_key)
-    return true
-  } catch (err) {
-    return false
-  }
-}
+// const isAuthenticatedConsumer = function (token: string): boolean {
+//   try {
+//     decryptApiKey(token, process.env.consumer_key)
+//     return true
+//   } catch (err) {
+//     return false
+//   }
+// }
 
-function decryptApiKey(text: string, passphrase: string) {
-  const key = generateKeyFromPassphrase(passphrase)
-  const textParts = text.split(':')
-  const iv = Buffer.from(textParts.shift(), 'hex')
-  const encryptedText = Buffer.from(textParts.join(':'), 'hex')
-  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
-  let decrypted = decipher.update(encryptedText)
-  decrypted = Buffer.concat([decrypted, decipher.final()])
-  return decrypted.toString()
-}
+// function decryptApiKey(text: string, passphrase: string) {
+//   const key = generateKeyFromPassphrase(passphrase)
+//   const textParts = text.split(':')
+//   const iv = Buffer.from(textParts.shift(), 'hex')
+//   const encryptedText = Buffer.from(textParts.join(':'), 'hex')
+//   const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
+//   let decrypted = decipher.update(encryptedText)
+//   decrypted = Buffer.concat([decrypted, decipher.final()])
+//   return decrypted.toString()
+// }
 
-function generateKeyFromPassphrase(passphrase: string) {
-  return crypto.scryptSync(passphrase, '10', 32)
-}
+// function generateKeyFromPassphrase(passphrase: string) {
+//   return crypto.scryptSync(passphrase, '10', 32)
+// }
 
-export { isAuthenticatedConsumer }
+// // export { isAuthenticatedConsumer }
--- server.ts
+++ server.ts
@@ -3,24 +3,25 @@ dotenv.config()
 import http from "http";
 import { Server } from "socket.io";
 import Subject from "./subject/Subject";
-import MqttSubject from './thirdPartySubject/MqttSubject';
+// import MqttSubject from './thirdPartySubject/MqttSubject';
 import Listener from "./listener/Listener";
 import type {IListener} from "./listener/IListener";
 import allowedAttrs from "./global/attrs";
-import {
-  isAuthenticatedConsumer,
-} from "./middleware/auth";
+// import {
+//   isAuthenticatedConsumer,
+// } from "./middleware/auth";
 
 const port = process.env.PORT || 2000;
 const app = http.createServer();
 const io = new Server(app, { cors: { origin: "*" } });
 
 const consumerNamespace = io.of("/consumer");
+const producerNamespace = io.of("/producer");
 
 const subject = Subject.getSubject();
 subject.setSocket(io);
-const mqttSubject = new MqttSubject(process.env.THIRDPARTY_HOST, { protocol: 'mqtt', port: Number(process.env.THIRDPARTY_PORT), username: process.env.THIRDPARTY_USER, password: process.env.THIRDPARTY_PASS });
-mqttSubject.setListener(subject);
+// const mqttSubject = new MqttSubject(process.env.THIRDPARTY_HOST, { protocol: 'mqtt', port: Number(process.env.THIRDPARTY_PORT), username: process.env.THIRDPARTY_USER, password: process.env.THIRDPARTY_PASS });
+// mqttSubject.setListener(subject);
 
 // consumerNamespace.use((socket, next) => {
 //   if (!isAuthenticatedConsumer(socket.handshake.auth.token)) return;
@@ -52,4 +53,18 @@ consumerNamespace.on("connection", (socket) => {
   });
 });
 
-app.listen(port, () => console.log("server started"));
+producerNamespace.on('connection', (socket) => {
+  console.log('producer connected', socket.id)
+  socket.on('device_update', (packet) => {
+    console.log('🚀 ~ socket.on ~ paacket:', packet)
+    subject.notifyListeners(packet)
+  })
+})
+
+app.on('request', (request, res) => {
+  if (request.url === '/health') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    return res.end('OK');
+  }
+})
+app.listen(port, () => console.log("server started", port));
